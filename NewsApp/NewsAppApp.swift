c26158a974b1dd8c0ded6416de6b83f52ffa55dd
Ahmad Maulana <42543638+ahmdmau@users.noreply.github.com>
2024-11-22 17:11:09 +0700

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by ICT Ahmad Maulana on 22/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
